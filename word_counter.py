8df7460a3b69f839c5260cb57a3d8f444abdc595
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-18 06:13:26 -0700

    Updated word_counter.py

--- word_counter.py
+++ word_counter.py
@@ -1,9 +1,13 @@
 #Python based Word counter
+#input text
 text = input("enter text:")
+#define words using split
 words = len(text.split())
+#defining char as length of text
 chars = len(text)
+#define logic for vowels
 vowels = sum(1 for c in text.lower() if c in "aeiou")
-
+#final step of printing everything
 print("words:",words)
 print("characters:",chars)
 print("vowels:",vowels)
